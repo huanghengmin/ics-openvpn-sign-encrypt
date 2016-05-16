c1c532304e343e0479f55c14dad5e548bb69e71b
hhm <465805947@QQ.com>
2016-05-16 17:29:17 +0800

    修改android VPN 客户端休眠问题，加入alarm唤醒

--- java/com/zd/vpn/alarm/AlarmSchedulingService.java
+++ java/com/zd/vpn/alarm/AlarmSchedulingService.java
@@ -1,15 +1,13 @@
 package com.zd.vpn.alarm;
 
 import android.app.IntentService;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v4.app.NotificationCompat;
 
-import com.zd.vpn.R;
-import com.zd.vpn.ui.MoreActivity;
+import android.util.Log;
+
+import java.util.Date;
 
 public class AlarmSchedulingService extends IntentService {
 
@@ -19,38 +17,17 @@ public class AlarmSchedulingService extends IntentService {
 
     public static final String TAG = "Scheduling Service";
 
-    public static final int NOTIFICATION_ID = 1;
-
-    private NotificationManager mNotificationManager;
-
-    private String readIp(Context context) {
-        SharedPreferences sPreferences = context.getSharedPreferences("com.zd.vpn", Context.MODE_PRIVATE);
-        String ip = sPreferences.getString("vpn.ip",null);
-        return  ip;
-    }
-
     @Override
     protected void onHandleIntent(Intent intent) {
-        String ip = readIp(getApplicationContext());
+        SharedPreferences sPreferences = getApplicationContext().getSharedPreferences("com.zd.vpn", Context.MODE_PRIVATE);
+        String ip = sPreferences.getString("vpn.ip",null);
+        String port = sPreferences.getString("vpn.port",null);
         if (ip != null) {
-            String result = PingUtils.ping(ip);
-            if (result == null) {
-               sendNotification("网关服务器请求失败！");
+            boolean result = ConnectUtils.connect(ip, Integer.parseInt(port));
+            if (!result) {
+                Log.e(TAG,"connect vpn server fail "+new Date());
             }
-            PingUtils.ping(ip);
         }
         AlarmReceiver.completeWakefulIntent(intent);
-    }
-
-    private void sendNotification(String msg) {
-        mNotificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent(this, MoreActivity.class), 0);
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.icon)
-                .setContentTitle(getString(R.string.ping_result))
-                .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
-                .setContentText(msg);
-        mBuilder.setContentIntent(contentIntent);
-        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
     }
 }
